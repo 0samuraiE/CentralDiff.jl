c13ba4a4820725468bda695b22393eb4546c9947
0samuraiE <102441200+0samuraiE@users.noreply.github.com>
2024-07-28 01:45:26 +0900

    first commit

--- src/CenterDiff.jl
+++ src/CenterDiff.jl
@@ -1,5 +1,122 @@
 module CenterDiff
+export Axis, XAxis, YAxis, ZAxis, dI, Order
+export TaylorMatrix, f̄c, dfdxc, dfdx, d2fdx, Simple, countorder
 
-# Write your package code here.
+import Base: +
 
+struct Order{N} end
+Order(N) = Order{N}()
+
+abstract type AbstractAxis end
+struct XAxis <: AbstractAxis end
+struct YAxis <: AbstractAxis end
+struct ZAxis <: AbstractAxis end
+
+struct dI{T<:AbstractAxis}
+    axis::T
+    stride::Int
+end
+
++(x::CartesianIndex{1}, y::dI{XAxis}) = x + CartesianIndex(y.stride)
++(x::CartesianIndex{2}, y::dI{XAxis}) = x + CartesianIndex(y.stride, 0)
++(x::CartesianIndex{2}, y::dI{YAxis}) = x + CartesianIndex(0, y.stride)
++(x::CartesianIndex{3}, y::dI{XAxis}) = x + CartesianIndex(y.stride, 0, 0)
++(x::CartesianIndex{3}, y::dI{YAxis}) = x + CartesianIndex(0, y.stride, 0)
++(x::CartesianIndex{3}, y::dI{ZAxis}) = x + CartesianIndex(0, 0, y.stride)
++(x::dI, y::CartesianIndex) = +(y, x)
+
+function TaylorMatrix(samples)
+    [i^j / factorial(j) for i in samples, j in 0:length(samples)-1]
+end
+
+function _generate_terms(C)
+    N = length(C)
+    [:($(Float64(C[n])) * F[I+dI(axis, $(n - cld(N, 2)))]) for n in 1:N]
+end
+
+function _shift_and_add(C1, C2)
+    N = length(C1)
+    ret = zeros(eltype(C1), 2N - 1)
+
+    for i in axes(C1, 1), j in axes(C2, 1)
+        ret[i+j-1] += C1[i] * C2[j]
+    end
+    ret
+end
+
+f̄c(O::Order{N}, F) where {N} = f̄c(O, XAxis(), F, CartesianIndex(div(N, 2)))
+dfdxc(O::Order{N}, F, dxi) where {N} = dfdxc(O, XAxis(), F, CartesianIndex(div(N, 2)), dxi)
+dfdx(O::Order{N}, F, dxi) where {N} = dfdx(O, XAxis(), F, CartesianIndex(N), dxi)
+d2fdx(O::Order{N}, F, dxi) where {N} = d2fdx(O, XAxis(), F, CartesianIndex(N), dxi)
+
+for N in (2, 4, 6, 8, 10, 12, 14, 16, 18, 20)
+    samples = Rational{BigInt}.((1-N)//2:(N-1)//2)
+    A = TaylorMatrix(samples)
+    Ai = inv(A)
+
+    @eval begin
+        @inline function f̄c(::Order{$N}, axis, F, I)
+            $(Expr(:call, :+, _generate_terms(@view(Ai[1, :]))...))
+        end
+
+        @inline function dfdxc(::Order{$N}, axis, F, I, dxi)
+            $(Expr(:call, :+, _generate_terms(@view(Ai[2, :]))...)) * dxi
+        end
+
+        @inline function dfdx(::Order{$N}, axis, F, I, dxi)
+            $(Expr(:call, :+, _generate_terms(@views(_shift_and_add(Ai[1, :], Ai[2, :])))...)) * dxi
+        end
+
+        @inline function d2fdx(::Order{$N}, axis, F, I, dxi)
+            $(Expr(:call, :+, _generate_terms(@views(_shift_and_add(Ai[2, :], Ai[2, :])))...)) * dxi^2
+        end
+    end
+end
+
+module Simple
+    using ..CenterDiff
+    using ..CenterDiff: _generate_terms
+
+    @inline dfdx(O::Order{N}, F, dxi) where {N} = dfdx(O, XAxis(), F, CartesianIndex(div(N, 2) + 1), dxi)
+    @inline d2fdx(O::Order{N}, F, dxi) where {N} = d2fdx(O, XAxis(), F, CartesianIndex(div(N, 2) + 1), dxi)
+
+    for N in (2, 4, 6, 8, 10, 12, 14, 16, 18, 20)
+        samples = Rational{BigInt}.(-div(N, 2):div(N, 2))
+        A = TaylorMatrix(samples)
+        Ai = inv(A)
+
+        @eval begin
+            @inline function dfdx(::Order{$N}, axis, F, I, dxi)
+                $(Expr(:call, :+, _generate_terms(@view(Ai[2, :]))...)) * dxi
+            end
+
+            @inline function d2fdx(::Order{$N}, axis, F, I, dxi)
+                $(Expr(:call, :+, _generate_terms(@view(Ai[3, :]))...)) * dxi^2
+            end
+        end
+    end
+end
+
+function _compute_orders(hs, errs)
+    log.(hs[2:end] ./ hs[1:end-1], errs[2:end] ./ errs[1:end-1])
+end
+
+function _countorder(hs, errslist::NTuple{N,NamedTuple}) where {N}
+    keys = propertynames(errslist[begin])
+    orderlist = map(keys) do key
+        es = getfield.(errslist, key)
+        _compute_orders(hs, es)
+    end
+    NamedTuple{keys}(orderlist)
+end
+
+function _countorder(hs, errslist::NTuple{N,Float64}) where {N}
+    es = errslist
+    _compute_orders(hs, es)
+end
+
+function countorder(f, hs...)
+    errslist = map(f, hs)
+    _countorder(hs, errslist)
+end
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,50 @@ using CenterDiff
 using Test
 
 @testset "CenterDiff.jl" begin
-    # Write your tests here.
+    _centered_coords(x0, h, N) = @. x0 + h * ((1-N)//2:(N-1)//2)
+
+    for O in (2, 4, 6, 8, 10, 12, 14, 16, 18, 20)
+        f̃(x) = exp(x)
+        df̃dx(x) = exp(x)
+        d2f̃dx(x) = exp(x)
+        x0 = 1.0
+
+        order = countorder(2, 1, 1 / 2) do h
+            X1 = _centered_coords(x0, h, O)
+            Y1 = f̃.(X1)
+
+            X2 = _centered_coords(x0, h, 2O - 1)
+            Y2 = f̃.(X2)
+
+            (;
+                f̄c=f̃(x0) - f̄c(Order(O), Y1),
+                dfdxc=df̃dx(x0) - dfdxc(Order(O), Y1, 1 / h),
+                dfdx=df̃dx(x0) - dfdx(Order(O), Y2, 1 / h),
+                d2fdx=d2f̃dx(x0) - d2fdx(Order(O), Y2, 1 / h),
+            )
+        end
+        all(order.f̄c .> O)
+        all(order.dfdxc .> O)
+        all(order.dfdx .> O)
+        all(order.d2fdx .> O)
+    end
+
+    for O in (2, 4, 6, 8, 10, 12, 14, 16, 18, 20)
+        f̃(x) = exp(x)
+        df̃dx(x) = exp(x)
+        d2f̃dx(x) = exp(x)
+        x0 = 1.0
+
+        order = countorder(2, 1, 1 / 2) do h
+            X = _centered_coords(x0, h, O + 1)
+            Y = f̃.(X)
+
+            (;#
+                dfdx=df̃dx(x0) - Simple.dfdx(Order(O), Y, 1 / h),
+                d2fdx=d2f̃dx(x0) - Simple.d2fdx(Order(O), Y, 1 / h),
+            )
+        end
+        all(order.dfdx .> O)
+        all(order.d2fdx .> O)
+    end
 end
